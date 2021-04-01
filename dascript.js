f2a4e6a1bd3051b993299091927b552cc7c545d8
TheASE2096 <81771532+TheASE2096@users.noreply.github.com>
2021-04-01 19:46:54 +0100

    Add files via upload

---
+++ dascript.js
@@ -0,0 +1,7 @@
+var dababywillturn=intoaconvertible
+
+function greenBackground(){
+    document.getElementById("text").style.backgroundColor = "blue";
+    console.log("ThisWorks")
+    console.log("1 + 2")
+}
